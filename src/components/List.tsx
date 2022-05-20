6e99aaf5426923e614241f810ea86ccdb86ab239
Mario Jhaziel Cortez Cortes <66147106+Mario1613@users.noreply.github.com>
2022-05-20 01:55:11 -0500

    limit4

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -8,7 +8,7 @@ const List = ({data}:any) => {
   const data = data.reverse();
   return (
     <article className={styles.container}>  
-      {data.slice(0,5).map((date:any): JSX.Element=>(
+      {data.slice(0,4).map((date:any): JSX.Element=>(
         <div className={styles.video} key={date.id} >
     
           <ReactPlayer url={date.videourl} width="100%" height="100%"  />
