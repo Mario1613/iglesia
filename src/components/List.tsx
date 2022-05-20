851cd41705867669d9c821e8c1a289df50a7e059
Mario Jhaziel Cortez Cortes <66147106+Mario1613@users.noreply.github.com>
2022-05-20 01:37:56 -0500

    updatefinish

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -5,11 +5,10 @@ import ReactPlayer from 'react-player';
 
 const List = ({data}:any) => {
 
-  const date = data.reverse();
   return (
     <article className={styles.container}>  
  
-      {date.reverse.map((date:any): JSX.Element=>(
+      {data.reverse.map((date:any): JSX.Element=>(
         <div className={styles.video} key={date.id} >
     
           <ReactPlayer url={date.videourl} width="100%" height="100%"  />
