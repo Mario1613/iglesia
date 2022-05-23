6d5ce2292f79e336094dc788620f3738adbc7d12
Mario1613 <mariojhaziel25@gmail.com>
2022-05-23 16:59:51 -0500

    ultimoscambios

--- src/views/Capellania/index.tsx
+++ src/views/Capellania/index.tsx
@@ -36,7 +36,7 @@ export default function Capellania() {
             <div className={styles.icon}>
              
                 <Link to="/">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-caret-left-fill" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="white" className="bi bi-caret-left-fill" viewBox="0 0 16 16">
                   <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
                 </svg>
                 </Link>
--- src/views/Nav/index.tsx
+++ src/views/Nav/index.tsx
@@ -52,6 +52,7 @@ function Nav (){
         <img src={Perfil} alt="Logo" className="imgsize"/>
         <div>
           <h2>Iván Alberto Cuenca Robles</h2>
+          <p style={{textAlign:'justify'}}>Es oriundo de Jamundí Valle en el país de Colombia, nacido el 25 de Septiembre de 1976, al nacer sus padres eran cristianos de la Iglesia del nombre de Jesús, es nacido en un hogar cristiano y toda su vida ha estado en los caminos del Señor Jesucristo, ha sido servidor en la Iglesia desde niño predicando en los cultos de niños, en la adolescencia, en la juventud y en su edad adulta, hace 16 años sirve en el santo ministerio en países como España y Estados Unidos, por la gracia del Señor Jesús ha podido cursar sus estudios como Lice. en Teología, Doctorado en Filosofía y Ciencias Religiosas,  diplomados en Teología, Consejería Familiar, Ciencia y Fe, Comunicación Radial y Televisión, Capellanía Internacional, Defensor en Derechos Humanos entre otros, Actualmente es pastor en Sarasota Florida en los Estados Unidos.</p>
          
         <button className="btnModal" onClick={() => setState(!state)} >Cerrar</button>
         </div>
@@ -191,7 +192,7 @@ Modal
     cursor: pointer;
     margin-top: 1rem;
     margin-left: 65%;
-
+    margin-bottom: 1rem;
   }
 
 
