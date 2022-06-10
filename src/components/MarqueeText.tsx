234049343f85537bc69861f7f018eb67031cf74d
Mario1613 <mariojhaziel25@gmail.com>
2022-06-09 19:48:38 -0500

    sn2target

--- src/components/MarqueeText.tsx
+++ src/components/MarqueeText.tsx
@@ -1,34 +1,27 @@
-import  { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const MarqueeText = () =>{
-
+const MarqueeText = () => {
     const [datas, setData] = useState([]);
-
-    useEffect(()=>{
-        const consultarAPI = async () =>{
-            try{
-            const url:string = 'https://dolphin-app-rn85f.ondigitalocean.app/textodelasemanas';
-            const respuesta = await fetch(url);
-            const resultado = await respuesta.json()
-            // console.log(resultado[0]);
+    useEffect(() => {
+        const consultarAPI = async () => {
+            try {
+                const url: string = 'https://dolphin-app-rn85f.ondigitalocean.app/textodelasemanas';
+                const respuesta = await fetch(url);
+                const resultado = await respuesta.json()
+                // console.log(resultado[0]);
                 setData(resultado.reverse());
-              }catch(error){
-                console.log('error') ;
-              }
+            } catch (error) {
+                console.log('error');
             }
-            consultarAPI();
-    },[]);
-
-    return(
-        <div>
-        { datas.map((data:any): JSX.Element =>(
-            <h3 key={data.id} >{data.textodelasemana}</h3>
-        )
-        
-        )
-        
         }
+        consultarAPI();
+    }, []);
 
+    return (
+        <div>
+            {datas.map((data: any): JSX.Element => (
+                <h3 key={data.id} >{data.textodelasemana}</h3>
+            ))}
         </div>
     )
 
