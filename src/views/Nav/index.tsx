152a0da626d657c9eb6604f77fa2cedfa3f71499
Mario1613 <mariojhaziel25@gmail.com>
2023-01-08 22:48:06 -0600

    mejora de modal

--- src/views/Nav/index.tsx
+++ src/views/Nav/index.tsx
@@ -1,21 +1,172 @@
 import styled from "styled-components";
 import Logo from "../Home/logo.png";
 import styles from "../Nav/styles/Nav.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import BurguerButton from "../../components/BurguerButton";
-
 import Perfil from "../Nav/Perfil.jpg";
 import { useState } from "react";
+const Header = styled.header`
+  .burguer {
+    display: none;
+    margin: -50px 86%;
+    @media (max-width: 768px) {
+      width: 33px;
+      height: 30px;
+      position: absolute;
+      cursor: pointer;
+      display: inline-block;
+    }
+  }
 
+  .barTwo {
+    background-color: var(--morado);
+  }
+  @media (max-width: 767px) {
+    .barTwo {
+      transform: translateX(-100%);
+      width: 80%;
+      height: 100%;
+      color: #4a5ea2;
+      background-color: var(--white);
+      transition-duration: 500ms;
+      position: fixed;
+      top: 0;
+      z-index: 2;
+    }
+    .barTwo.active {
+      height: 100%;
+      position: fixed;
+      display: block;
+      transform: translateX(0);
+    }
+  }
+
+  /*
+
+Modal
+*/
+  .biografia {
+    display: none;
+    transform: translateX(-100%);
+    transition-duration: 500ms;
+  }
+  @media (min-width: 768px) {
+    .biografia {
+      display: block;
+      transform: translateX(100%);
+      transition-duration: 500ms;
+      position: fixed;
+      z-index: 3;
+      overflow-y: scroll;
+      background: white;
+      margin-top: 2rem;
+      margin-left: 69vw;
+      width: 31vw;
+      height: 70vh;
+      font-size: 1.2rem;
+      font-weight: bold;
+      padding: 1rem 0;
+      transition: all 3s ease;
+      box-shadow: -4px 5px 8px #00000048;
+    }
+  }
+
+  .biografia.active {
+    transform: translateX(0);
+    height: 100%;
+    position: fixed;
+    overflow-y: scroll;
+    display: block;
+    background: white;
+    width: 80%;
+    margin-top: -52px;
+    padding-top: 1rem;
+    z-index: 3;
+
+    @media (min-width: 768px) {
+      margin-top: 2rem;
+      margin-left: 69vw;
+      width: 30vw;
+      height: 70vh;
+      font-size: 1.2rem;
+      font-weight: bold;
+      padding: 1rem 0;
+      box-shadow: -4px 5px 8px #00000048;
+    }
+  }
+  .buttonEmergente {
+    display: none;
+  }
+  @media (min-width: 768px) {
+    .visible {
+      display: none;
+    }
+  }
+  @media (min-width: 768px) {
+    .buttonEmergente {
+      box-shadow: -4px 5px 8px #00000048;
+      cursor: pointer;
+      width: 6rem;
+      position: fixed;
+      display: flex;
+      right: 0rem;
+      height: 2rem;
+      background: linear-gradient(to right, #f17a11, #ffb20d);
+      border-start-start-radius: 1rem;
+      border-end-start-radius: 1rem;
+      transition: all 3s ease;
+      align-items: center;
+      padding-left: 1rem;
+      z-index: 3;
+    }
+    .textoButtonEmergente {
+      color: white;
+      font-weight: 600;
+      letter-spacing: 1px;
+      transition-duration: 500ms;
+    }
+    .textoButtonEmergente.blur {
+      display: flex;
+    }
+    .buttonEmergente.blur {
+      padding-right: 30vw;
+    }
+  }
+  .imgSize {
+    width: 100%;
+    border-radius: 5px;
+    @media(min-width: 768px){
+      float: left;
+      padding: .5rem 1rem 1rem .5rem;
+      width: 45%;
+    }
+  }
+  .sectionTexto{
+  
+  }
+
+  .parrafo {
+    max-width: 90%;
+
+    margin: 0 auto;
+    line-height: 1.5;
+    /* padding: 1rem; */
+    @media(min-width:768px){
+
+    }
+  }
+`;
 function Nav() {
   const [state, setState] = useState<boolean>(false);
   const [clicked, setClicked] = useState<Boolean>(false);
+  const [blur, setBlur] = useState(false);
+  const navigate = useNavigate();
 
   return (
     <Header>
       <div className={styles.barOne}>
-        <LogoStyled src={Logo} alt="Logo" />
-        <h1 className={styles.H1Principal}>
+        <LogoStyled src={Logo} alt="Logo" onClick={() => navigate("/")} />
+        <h1 className={styles.H1Principal} onClick={() => navigate("/")}>
           Iglesia Pentecostal Del Nombre De Jesús
         </h1>
       </div>
@@ -32,15 +183,16 @@ function Nav() {
               libros
             </Link>
           </Navegacion>
-
-          <Navegacion>
-            <button
-              onClick={() => setState(!state)}
-              className={styles.navegacionc}
-            >
-              Pastor
-            </button>
-          </Navegacion>
+          <div className="visible">
+            <Navegacion>
+              <button
+                onClick={() => setState(!state)}
+                className={styles.navegacionc}
+              >
+                Pastor
+              </button>
+            </Navegacion>
+          </div>
 
           <Navegacion>
             <a href="#nosotros" className={styles.navegacion}>
@@ -55,18 +207,25 @@ function Nav() {
           </Navegacion>
         </UnorderedList>
       </nav>
+      <div
+        className={`buttonEmergente ${blur && "blur"}`}
+        onClick={() => setBlur(!blur)}
+      >
+        <h4 className={`textoButtonEmergente ${blur && "blur"}`}>Pastor</h4>
+      </div>
       <div className="burguer">
         <BurguerButton
           clicked={clicked}
           handleClick={() => {
-            setState(false)
-            setClicked(!clicked)}}
+            setState(false);
+            setClicked(!clicked);
+          }}
         />
       </div>
 
-      <div className={`biografia ${state ? "active" : ""}`}>
-        <div className="minContainer">
-          <img src={Perfil} alt="Logo" className="imgsize" />
+      <div className={`biografia ${blur || state ? "active" : ""}`}>
+        <div className="parrafo">
+          <img src={Perfil} alt="Logo" className="imgSize" />
           <div className="sectionTexto">
             <h3>Iván Alberto Cuenca Robles</h3>
             <p style={{ textAlign: "justify" }}>
@@ -84,10 +243,6 @@ function Nav() {
               Defensor en Derechos Humanos entre otros, Actualmente es pastor en
               Sarasota Florida en los Estados Unidos.
             </p>
-
-            <button className="btnModal" onClick={() => setState(!state)}>
-              Cerrar
-            </button>
           </div>
         </div>
       </div>
@@ -96,126 +251,6 @@ function Nav() {
 }
 
 export default Nav;
-
-const Header = styled.header`
-  .burguer {
-    display: none;
-    margin: -50px 86%;
-    @media(max-width: 768px){
-      width: 33px;
-      height: 30px;
-      position: absolute;
-      cursor: pointer;
-      display: inline-block;
-    }
-  }
-
-  .barTwo {
-    background-color: var(--morado);
-  }
-  @media (max-width: 767px) {
-    .barTwo {
-      /* margin-top: -52px; */
-      transform: translateX(-100%);
-      width: 80%;
-      height: 100%;
-      color: #4a5ea2;
-      background-color: var(--white);
-      transition-duration: 500ms;
-      position: fixed;
-      top:0;
-      z-index: 2;
-    }
-    .barTwo.active {
-      height: 100%;
-      /* position: relative; */
-      position: fixed;
-      display: block;
-      transform: translateX(0);
-    }
-  }
-
-  /*
-
-Modal
-*/
-  .biografia {
-    display: none;
-    transform: translateX(-100%);
-    transition-duration: 500ms;
-  }
-  .imgsize {
-    width: 100%;
-    grid-area: "img";
-  }
-  .sectionTexto {
-    grid-area: "texto";
-  }
-
-  .biografia.active {
-    transform: translateX(0);
-    height: 100%;
-    position: fixed;
-    overflow-y: scroll;
-    
-    display: block;
-    background: white;
-    width: 80%;
-
-    margin-top: -52px;
-    padding-top: 1rem;
-    z-index: 2;
-
-    @media (min-width: 768px) {
-      position: absolute;
-      margin-top: .01rem;
-      margin-left: 69vw;
-      width: 30vw;
-      height: 70vh;
-      /* margin-left: 26%; */
-      font-size: 1.2rem;
-      font-weight: bold;
-      padding: 1rem 0;
-      box-shadow: -4px 5px 8px #00000048;
-    }
-  
-  
-  }
-  .minContainer {
-    display: grid;
-    grid-gap: 1rem;
-    grid-template-areas:
-      "img img img " /* Primera Fila */
-      "texto" "texto" "texto"
-      "texto" "texto" "texto"
-      "texto" "texto" "texto";
-    /* grid-template-columns: 1fr; */
-    max-width: 100%;
-    margin: 0 auto;
-    padding: 1rem;
-    .btnModal {
-      background-color: var(--morado);
-      color: var(--white);
-      border: none;
-      border-radius: 5px;
-      padding: 0.5rem 1rem;
-      font-size: 1rem;
-      font-weight: bold;
-      cursor: pointer;
-      margin-top: 1rem;
-      margin-left: 65%;
-      margin-bottom: 1rem;
-    }
-
-    @media (min-width: 768px) {
-      grid-template-areas:
-        "img texto texto " /* Primera Fila */
-        "img" "texto" "texto"
-        "texto" "texto" "texto"
-        "texto" "texto" "texto";
-    }
-  }
-`;
 
 const Navegacion = styled.li`
   list-style: none;
@@ -246,6 +281,7 @@ const UnorderedList = styled.ul`
 `;
 
 const LogoStyled = styled.img`
+  cursor: pointer;
   @media (max-width: 500px) {
     display: none;
   }
